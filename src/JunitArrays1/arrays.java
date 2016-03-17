3288378302ff03c69796a346c73cb37ad77083e1
Neel Kowdley <nkowdley@gmail.com>
2016-03-17 00:29:15 -0400

    finish tests

--- src/JunitArrays1/arrays.java
+++ src/JunitArrays1/arrays.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.lang.Object;
+import org.apache.commons.lang3.ArrayUtils;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -19,13 +21,15 @@ public class arrays {
 		Random rand = new Random(); /*Initialize a Random Number Generator*/
 		/*Create the arrays*/
 		for (int i=0;i<arrayNum;i++) {
-			int size=rand.nextInt(Integer.MAX_VALUE/500); /*Arrays can be of any size that is a positive int, but this causes a memory out of bounds exception, so instead we divide by 500*/
+			int size=rand.nextInt(Integer.MAX_VALUE/100000); /*Arrays can be of any size that is a positive int, but this causes a memory out of bounds exception, so instead we divide by 500*/
+			//int size=rand.nextInt(10);
 			int[] currentArray= new int[size]; /*Create the Array*/
 			/*fill the array*/
 			for(int j=0;j<size;j++) {
 				currentArray[j]=rand.nextInt(); /*We can add any number that is an int to the array*/
 			}
 			allArrays.add(currentArray); /*Add the arrays to the ArrayList*/
+			System.out.println(size);
 		}
 	}
 
@@ -55,31 +59,35 @@ public class arrays {
 			assertArrayEquals(currentArray,currentArrayCopy); /*Assert the Arrays are equal*/
 		}
 	}
+
 	/*Verify that the unsorted and sorted arrays both contain the same elements*/
 	@Test
 	public void testSameElements() {
-		fail("yo");
 		for (int i=0;i<arrayNum;i++){
 			int[] currentArray=allArrays.get(i); /*Get the current array*/
 			int[] currentArrayCopy=currentArray; /*copy the current array so that we can compare the sorted to the unsorted*/
 			Arrays.sort(currentArray);
-			//for (int j=0;j<currentArray.length;j++)
-			//{
-				//if
-			//}
-
+			for (int j=0;j<currentArray.length;j++) {
+				if( ArrayUtils.contains(currentArrayCopy,currentArray[j]) ) {
+					currentArrayCopy=ArrayUtils.removeElement(currentArrayCopy, currentArray[j]);
+				}
+			}
+			assertEquals(currentArrayCopy.length,0);
 		}
 	}
+
 	/*Verify that the Array is actually sorted*/
 	@Test
 	public void testActuallySorted() {
 		for (int i=0;i<arrayNum;i++){
 			int[] currentArray=allArrays.get(i); /*Get the current array*/
-			Arrays.sort(currentArray);
-			for (int j=1;j<currentArray.length;j++){
+			Arrays.sort(currentArray); /*Sort the Array*/
+			/*Make sure that each element in the array is bigger than the previous element*/
+			for (int j=1;j<currentArray.length;j++) {
 				assertTrue(currentArray[j]>=currentArray[j-1]);
 			}
 		}
+
 	}
 
 }
